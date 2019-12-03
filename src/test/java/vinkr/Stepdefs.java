f2361c54b9baa388962d5549ac045e1505234b54
pekkap <pekka.prepula@gmail.com>
2019-12-03 18:37:43 +0200

    help-storyn vaatimusmäärittely ja testit cucumberissa

--- src/test/java/vinkr/Stepdefs.java
+++ src/test/java/vinkr/Stepdefs.java
@@ -9,6 +9,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
+import org.junit.After;
 import static org.junit.Assert.*;
 
 public class Stepdefs {
@@ -24,9 +25,15 @@ public class Stepdefs {
     }
     
     @Given("komento {string} annetaan ohjelmalle")
-    public void komentoLisaaValittu(String komento) throws Throwable {
+    public void komentoValittu(String komento) throws Throwable {
         
         input += komento + "\n";
+    }
+    
+    @When("komento suoritetaan")
+    public void komentoSuoritetaan() throws Throwable {
+        input += "lopeta\n";
+        luoUIjaStreamit();
     }
     
     @When("otsikko {string}, kirjoittaja {string} ja ISBN {string} annetaan")
@@ -35,20 +42,23 @@ public class Stepdefs {
         input += otsikko + "\n";
         input += kirjoittaja + "\n";
         input += isbn + "\n";
-        input += "lopeta\n";
-
+        input += "lopeta" + "\n";
+        
+        luoUIjaStreamit();
+    }    
+    
+    @Then("ohjelmaan tulostuu {string}")
+    public void ohjelmaVastaaHalutullaTulosteella(String odotettuTuloste) throws UnsupportedEncodingException {
+ 
+        assertTrue(uiOutput.toString("UTF-8").contains(odotettuTuloste));
+    }  
+    
+    private void luoUIjaStreamit() {
         uiInput = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
         uiOutput = new ByteArrayOutputStream();
        
         app = new Vinkr();
         ui = new TextUI(app, uiInput, uiOutput);
         ui.run();
-    }    
-    
-    @Then("ohjelma vastaa tulosteella {string}")
-    public void ohjelmaVastaaHalutullaTulosteella(String odotettuTuloste) throws UnsupportedEncodingException {
-        
-        assertTrue(uiOutput.toString("UTF-8").contains(odotettuTuloste));
-    }  
-
+    }
 }
