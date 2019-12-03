e3601d4d8dccc48fc0f80be1f1cbff4a92d71000
PPeltola <murderbanana@gmail.com>
2019-12-03 19:23:09 +0200

    Lisättiin käyttöliittymä videoiden ja artikkelien lisäämiselle

--- src/main/java/vinkr/TextUI.java
+++ src/main/java/vinkr/TextUI.java
@@ -1,10 +1,15 @@
 package vinkr;
 
+import vinkr.vinkit.ArtikkeliVinkki;
 import vinkr.vinkit.KirjaVinkki;
 import vinkr.vinkit.Vinkki;
+import vinkr.vinkit.YoutubeVinkki;
+
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Scanner;
 
 public class TextUI {
@@ -36,16 +41,16 @@ public class TextUI {
 
     private void listaaKomennot() {
         output.println("Kaikki käytettävissä olevat komennot:");
-        output.println("apua: Tulostaa tämän listan uudestaan");
-        output.println("lisaa: Lisää kirja");
-        output.println("listaa: Listaa kaikki lukuvinkit");
-        output.println("lopeta: Sulkee sovelluksen");
+        output.println("  apua: Tulostaa tämän listan uudestaan");
+        output.println("  lisaa: Lisää uusi lukuvinkki");
+        output.println("  listaa: Listaa kaikki lukuvinkit");
+        output.println("  lopeta: Sulkee sovelluksen");
     }
 
     private void kasitteleKomento(String komento) {
         switch (komento) {
             case "lisaa":
-                lisaaKirja();
+                lisaaVinkki();
                 break;
             case "listaa":
                 listaaVinkit();
@@ -61,6 +66,62 @@ public class TextUI {
     private void tulostaVinkkiApukomennosta() {
         output.println();
         output.println("Jos tahdot nähdä kaikki komennot, kirjoita 'apua'");
+    }
+
+    private URL kysyUrl() {
+        while (true) {
+            String osoite = getInput("Videon osoite");
+
+            try {
+                URL url = new URL(osoite);
+                if (true) { //validoija.validoiUrl
+                    return url;
+                } else {
+                    output.println("Virhe: Anna kelvollinen URL-osoite");
+                }
+            } catch (MalformedURLException e) {
+                output.println("Virhe: Anna kelvollinen URL-osoite");
+            }
+        }
+    }
+
+    private void lisaaVinkki() {
+        output.println("Tuetut vinkkityypit ovat 'kirja', 'youtube' ja 'artikkeli'" + "\n" + "Takaisin pääset kirjoittamalla 'takaisin'");
+        String tyyppi = getInput("Vinkin tyyppi");
+        switch (tyyppi) {
+            case "kirja":
+                lisaaKirja();
+                break;
+            case "youtube":
+                lisaaVideo();
+                break;
+            case "artikkeli":
+                lisaaArtikkeli();
+                break;
+            case "takaisin":
+                break;
+            default:
+                output.println("Vinkin tyyppiä ei tunnistettu");
+                lisaaVinkki();
+        }
+    }
+
+    private void lisaaVideo() {
+        URL url = kysyUrl();
+        String otsikko = kysyOtsikko();
+        String kanava = kysyKanava();
+        YoutubeVinkki youtubeVinkki = new YoutubeVinkki(url, otsikko, kanava);
+        app.lisaaVinkki(youtubeVinkki);
+        output.println("Video lisätty");
+    }
+
+    private void lisaaArtikkeli() {
+        String otsikko = kysyOtsikko();
+        String kirjoittaja = kysyKirjoittaja();
+        URL url = kysyUrl();
+        ArtikkeliVinkki artikkeliVinkki = new ArtikkeliVinkki(url, otsikko, kirjoittaja);
+        app.lisaaVinkki(artikkeliVinkki);
+        output.println("Artikkeli lisätty");
     }
 
     private void lisaaKirja() {
@@ -79,6 +140,17 @@ public class TextUI {
                 return otsikko;
             } else {
                 output.println("Virhe: Otsikko ei saa olla tyhjä");
+            }
+        }
+    }
+
+    private String kysyKanava() {
+        while (true) {
+            String kanava = getInput("Youtube-kanava");
+            if (true) { //validoija.validoiKanava
+                return kanava;
+            } else {
+                output.println("Virhe: Anna kelvollinen Youtube-kanava");
             }
         }
     }
