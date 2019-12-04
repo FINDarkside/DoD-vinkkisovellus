5ca924e78e6ed22c90ede0482aa1ac3310937fc9
Jesse Niininen <findarkside@gmail.com>
2019-12-04 04:03:57 +0200

    Add test for serialization

--- src/test/java/vinkr/VinkrTest.java
+++ src/test/java/vinkr/VinkrTest.java
@@ -1,8 +1,11 @@
 package vinkr;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
 import org.junit.*;
 import static org.junit.Assert.*;
-import vinkr.vinkit.KirjaVinkki;
+import vinkr.vinkit.*;
 
 public class VinkrTest {
 
@@ -17,5 +20,19 @@ public class VinkrTest {
     public void vinkinLisaysOnnistuu() {
         vinkr.lisaaVinkki(new KirjaVinkki("Refactoring", "Fowler, Martin", "0201485672"));
         assertEquals(vinkr.getVinkit().size(), 1);
+    }
+
+    @Test
+    public void serialisointiJaDeserializointiToimii() throws MalformedURLException {
+        vinkr.lisaaVinkki(new ArtikkeliVinkki(new URL("https://stackoverflow.com/"), "StackOverflow", "Joku"));
+        vinkr.lisaaVinkki(new KirjaVinkki("Refactoring", "Fowler, Martin", "0201485672"));
+        vinkr.lisaaVinkki(new YoutubeVinkki(new URL("https://www.youtube.com/watch?v=P_nj6wW6Gsc"), "Kerbal Space Program 2 Cinematic Announce Trailer", "Kerbal Space Program"));
+        String json = vinkr.serialisoi();
+        Vinkr vinkr2 = Vinkr.deserialisoi(json);
+        List<Vinkki> vinkit = vinkr2.getVinkit();
+        assertEquals(vinkit.get(0).toString(), vinkr.getVinkit().get(0).toString());
+        assertEquals(vinkit.get(1).toString(), vinkr.getVinkit().get(1).toString());
+        assertEquals(vinkit.get(2).toString(), vinkr.getVinkit().get(2).toString());
+
     }
 }
